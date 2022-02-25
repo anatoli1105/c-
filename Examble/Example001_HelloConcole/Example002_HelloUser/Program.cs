fd67dd23affacd827d77c025505ff302e9f9f516
anatolii <anatoliise@mail.ru>
2022-02-25 13:15:10 +0300

    add Example005_sum

--- Examble/Example001_HelloConcole/Example002_HelloUser/Program.cs
+++ Examble/Example001_HelloConcole/Example002_HelloUser/Program.cs
@@ -1,6 +1,3 @@
-﻿Console.WriteLine("введите ваше имя");
-string username = Console.ReadLine();
-Console.WriteLine("привет,");
-Console.WriteLine(username);
+﻿
 
 
--- Examble/Example001_HelloConcole/Program.cs
+++ Examble/Example001_HelloConcole/Program.cs
@@ -1,6 +1,9 @@
-﻿string username = Console.ReadLine();
-Console.WriteLine("привет,");
-Console.WriteLine(username);
+﻿int numberA = new Random().Next(1, 10);//1 2 3 4 ... 9
+int numberB = new Random().Next(1, 10);
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+
 
 
 
